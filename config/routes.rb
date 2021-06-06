d28744272f6a5b1b55f8d8c323e46df0b47a62ea
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-06-06 16:20:47 +0000

    Add the User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'users#index'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
