08fdd0ec9f28ad370d0cd0c8f82363ea4651fb67
Rachael Wright-Munn <chaelcodes@gmail.com>
2021-06-13 16:55:08 +0000

    Add first spec

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'Users#show' do
+  let(:user_profile) { User.create(name: 'Sample', bio: 'This is a bio', confirmed_at: 1.minute.ago, email: 'chaelcodes@example.com', password: 'P@55w0rd') }
+
+  before(:each) do
+    sign_in user if user
+    visit "/users/#{user_profile.id}".dup
+  end
+
+  context 'when no user logged in' do
+    let(:user) {}
+
+    it 'demands signup' do
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+      expect(page).to_not have_content user_profile.name
+    end
+  end
+
+  context 'logged_in user' do
+    let(:user) { User.create(name: 'Sample', bio: 'This is a bio', confirmed_at: 1.minute.ago) }
+
+    it 'shows basic user information' do
+      expect(page).to have_content user_profile.name
+    end
+  end
+
+  context 'unconfirmed user' do
+    let(:user) { User.create(name: 'Sample', bio: 'This is a bio', confirmed_at: nil) }
+
+    it 'prompts the user to confirm email' do
+      expect(page).to have_content 'You have to confirm your email address before continuing.'
+      expect(page).not_to have_content user_profile.name
+    end
+  end
+end
