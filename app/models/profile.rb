56f2e332c3b47fee1d9c08c7a1ddfaf2aff06ff3
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-07-25 20:51:36 +0000

    Establish relationship between Events and Profiles through EventAttendees

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,9 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
+  has_many :event_attendees
+  has_many :events, through: :event_attendees
+
   validates :handle, presence: true
 
   def to_s
