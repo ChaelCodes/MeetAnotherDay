b180ac2a349f221a065e5d0357006a3b4743ebe4
Jack M <exegete@hey.com>
2021-07-25 19:54:23 -0700

    rubocop --auto-complete

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :event do
-    name { 'RubyConf' }
-    description { '[RubyConf 2020](https://rubyconf.org/) will be held in Denver.' }
-    start_at { '2021-11-08 00:00:00' }
-    end_at { '2021-11-10 00:00:00' }
+    name { "RubyConf" }
+    description { "[RubyConf 2020](https://rubyconf.org/) will be held in Denver." }
+    start_at { "2021-11-08 00:00:00" }
+    end_at { "2021-11-10 00:00:00" }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@
 
 FactoryBot.define do
   factory :user do
-    bio { 'Hello. I am dev. Friends please? Ty.' }
+    bio { "Hello. I am dev. Friends please? Ty." }
     sequence(:email) { |i| "buddy#{i}@example.com" }
-    name { 'Chael' }
-    password { 'P@55w0rd' }
+    name { "Chael" }
+    password { "P@55w0rd" }
     confirmed_at { 1.day.ago }
 
     trait :unconfirmed do
