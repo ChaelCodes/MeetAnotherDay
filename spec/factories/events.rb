06f8ea6525fc5074c249c94203d8c4af1e3fa4ef
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-08-01 18:33:26 +0000

    Add one_day event Factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -6,5 +6,11 @@ FactoryBot.define do
     description { "[RubyConf 2020](https://rubyconf.org/) will be held in Denver." }
     start_at { "2021-11-08 00:00:00" }
     end_at { "2021-11-10 00:00:00" }
+
+    trait :one_day do
+      name { "HexDevs Open-Source" }
+      start_at { "2021-08-05 21:00:00" }
+      end_at { "2021-08-05 23:59:00" }
+    end
   end
 end
