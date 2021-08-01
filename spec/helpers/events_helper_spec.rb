b99036f582f0a9f1a5632a76a59d057023ce1291
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-08-01 18:33:26 +0000

    Add one_day event Factory

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -11,10 +11,7 @@ RSpec.describe EventsHelper, type: :helper do
     it { is_expected.to eq "November 08, 2021 - November 10, 2021" }
 
     context "when 1 day event" do
-      let(:event) do
-        create :event, name: "HexDevs Open-Source",
-                       start_at: "2021-08-05 21:00:00", end_at: "2021-08-05 23:59:00"
-      end
+      let(:event) { create :event, :one_day }
 
       it { is_expected.to eq "August 05, 2021 21:00-23:59" }
     end
