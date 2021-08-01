2129628f270c718f7f9d5ac8f351cfe57ab873c4
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-08-01 19:22:54 +0000

    Allow users to attend an event from the event show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :profile, dependent: :destroy
 end
