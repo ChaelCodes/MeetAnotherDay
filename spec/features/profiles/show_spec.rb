b301a630a3382985fc413da54d746f76970e943d
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-08-03 21:16:18 +0000

    Allow Admin to Destroy, but not Edit Profiles

--- spec/features/profiles/show_spec.rb
+++ spec/features/profiles/show_spec.rb
@@ -17,8 +17,28 @@ describe "Profile" do
       visit "/profiles/#{profile.id}".dup
     end
 
-    it "shows the PROFILE" do
+    it "shows the profile" do
       expect(page).to have_content "ChaelCodes"
+      expect(page).not_to have_link "Edit", href: edit_profile_path(profile)
+      expect(page).not_to have_link "Delete", href: profile_path(profile)
+    end
+
+    context "when profile belongs to the user" do
+      let(:user) { profile.user }
+
+      it "allows user to edit profile" do
+        expect(page).to have_link "Edit", href: edit_profile_path(profile)
+        expect(page).to have_link "Delete", href: profile_path(profile)
+      end
+    end
+
+    context "when user is admin" do
+      let(:user) { create :user, :admin }
+
+      it "allows user to destroy profile" do
+        expect(page).not_to have_link "Edit", href: edit_profile_path(profile)
+        expect(page).to have_link "Delete", href: profile_path(profile)
+      end
     end
   end
 end
