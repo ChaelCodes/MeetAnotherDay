d12501611bce475cf341eaa43a12e30022e0064c
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-08-29 15:56:31 +0000

    Create the concept of Friendship!

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :event_attendees
   root "events#index"
 
   devise_for :users
 
   resources :events
+  resources :event_attendees
+  resources :friendships
   resources :profiles
   resources :users, except: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ spec/factories/friendships.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :friendship do
+    friendship_id { nil }
+    buddy_id { nil }
+    status { 1 }
+  end
+end
