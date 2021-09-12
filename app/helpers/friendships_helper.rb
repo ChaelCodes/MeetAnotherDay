83442fc78bb19329a124ed277299c2cb574dbc72
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-09-12 20:57:42 +0000

    Accept Friend Requests

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -2,12 +2,38 @@
 
 # methods to help with displaying friendship!
 module FriendshipsHelper
-  def friendship_button(profile)
-    return unless current_user.profile&.id
+  def friendship_button(profile, my_profile = current_user&.profile)
+    return unless my_profile.present?
+    friendship = my_profile.friendship_with(profile)
+    case friendship&.status
+    when nil
+      request_friend(profile, my_profile)
+    when "accepted"
+      link_to "✨Friends✨", friendship_path(friendship)
+    when "declined"
+      "Request Declined"
+    when "requested", "ignored"
+      if my_profile.id == friendship.friend_id
+        be_my_buddy_button(friendship)
+      else
+        "Request Sent"
+      end
+    end
+  end
+
+  def be_my_buddy_button(friendship)
+    button_to "Be my buddy?", friendship_path(friendship, { status: :accepted }), method: :put, class: 'btn btn-primary'
+  end
+
+  def decline_friendship_button(friendship)
+    button_to "Ignore", friendship_path(friendship), method: :put, class: 'btn btn-danger'
+  end
+
+  def request_friend(profile, my_profile)
     button_to "Request Friend",
               friendships_path(friendship: {
                                  friend_id: profile.id,
-                                 buddy_id: current_user.profile.id
+                                 buddy_id: my_profile.id
                                }),
               method: :post,
               class: "button is-primary"
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class Friendship < ApplicationRecord
+  # I asked friend to be my buddy
   belongs_to :buddy, class_name: "Profile"
+  # I was asked to become a friend by my buddy
   belongs_to :friend, class_name: "Profile"
 
-  enum status: { accepted: 0, declined: 1, requested: 2 }
+  enum status: { accepted: 0, declined: 1, ignored: 3, requested: 2 }
 
   validates :status, presence: true
 end
