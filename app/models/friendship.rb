e2b8bea8654f20474b5e207c651abce0e8fdf39c
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-09-12 21:06:35 +0000

    Update the index page

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -10,4 +10,10 @@ class Friendship < ApplicationRecord
   enum status: { accepted: 0, blocked: 1, requested: 2 }
 
   validates :status, presence: true
+
+  # Returns nil if they're not one of the friends
+  def not_my_profile(profile)
+    return friend if profile.id == buddy_id
+    buddy if profile.id == friend_id
+  end
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe Friendship, type: :model do
   let(:friendship) { create(:friendship) }
 
   it { expect(friendship).to be_valid }
+
+  describe "#not_my_profile" do
+    subject { friendship.not_my_profile(profile) }
+
+    let!(:profile) { create(:profile) }
+
+    context "when I am the buddy" do
+      let!(:friend) { create(:profile) }
+      let(:friendship) { create(:friendship, buddy: profile, friend: friend) }
+
+      it { is_expected.to eq(friend) }
+    end
+
+    context "when I am the friend" do
+      let!(:buddy) { create(:profile) }
+      let!(:friendship) { create(:friendship, buddy: buddy, friend: profile) }
+
+      it { is_expected.to eq(buddy) }
+    end
+
+    context "when I am the third wheel" do
+      it { is_expected.to be_nil }
+    end
+  end
 end
