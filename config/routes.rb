ade54cb91508d4becb669ffc7a4247fd9503b975
Francisco Paradella <franciscoleite.dev@protonmail.com>
2021-10-05 21:27:57 -0300

    Add support controller and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "events#index"
+  root "support#about"
 
   devise_for :users
 
+  resources :support
   resources :events
   resources :event_attendees
   resources :friendships
   resources :profiles
   resources :users, except: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/about", to: "support#about"
 end
