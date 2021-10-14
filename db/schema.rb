43856429932ea29ed19a3f3c501373a248d6344d
Yago Santos <yagosansz@gmail.com>
2021-10-13 21:48:23 -0400

    refactor: Add a handle column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_022040) do
+ActiveRecord::Schema.define(version: 2021_10_14_013357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_10_06_022040) do
     t.datetime "end_at", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "handle"
   end
 
   create_table "friendships", force: :cascade do |t|
