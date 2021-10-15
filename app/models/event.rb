e8587cee98314409c7d18794067d641b9aa5d904
Yago Santos <yagosansz@gmail.com>
2021-10-14 22:16:59 -0400

    refactor: Add uniqueness and url format validation to handle

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,9 @@ class Event < ApplicationRecord
   has_many :attendees, through: :event_attendees, source: :profile
 
   validates :start_at, :end_at, presence: true
-  validates :handle, presence: true
+  validates :handle, presence: true,
+                     format: { with: /\A[a-zA-Z0-9]+\z/, message: "Only letters and numbers are allowed" },
+                     uniqueness: { case_sensitive: true }
 
   scope :ongoing_or_upcoming, -> { where("end_at >= ?", Time.zone.now) }
 
