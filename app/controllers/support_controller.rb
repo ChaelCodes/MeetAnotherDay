14e1cc2511d3d37d52a9626a09a5e406d998994c
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-10-17 19:15:44 +0000

    Document the support controller

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# This controller is for general application info pages.
+# It implements the about page, documentation, and other info pages.
 class SupportController < ApplicationController
   # GET /about
   def about; end
