8a64d3fe37937d76b4a5e4d6260fed43c833b07e
kemal akın <kmlakin97@gmail.com>
2021-10-23 00:00:58 +0300

    #92 Add user sign condition to new event button (#95)
    * Add user sign condition to new event button
    * Spec refactor new event button condition
    * Button condition converted Pundit check

--- spec/features/events/index_spec.rb
+++ spec/features/events/index_spec.rb
@@ -13,6 +13,7 @@ describe "Events" do
 
   it "shows the event" do
     expect(page).to have_link "RubyConf", href: event_path(event)
+    expect(page).not_to have_link "New Event", href: new_event_path
     expect(page).not_to have_link "Edit", href: edit_event_path(event)
     expect(page).not_to have_button "Delete"
   end
@@ -22,11 +23,23 @@ describe "Events" do
     expect(page).not_to have_link past_event.name, href: event_path(past_event)
   end
 
+  context "when user logged in but unconfirmed email" do
+    let(:user) { create :user, confirmed_at: nil, confirmation_sent_at: 1.day.ago }
+
+    it "shows the event" do
+      expect(page).to have_link "RubyConf", href: event_path(event)
+      expect(page).not_to have_link "New Event", href: new_event_path
+      expect(page).not_to have_link "Edit", href: edit_event_path(event)
+      expect(page).not_to have_button "Delete"
+    end
+  end
+
   context "when user logged in" do
     let(:user) { create :user }
 
     it "shows the event" do
       expect(page).to have_link "RubyConf", href: event_path(event)
+      expect(page).to have_link "New Event", href: new_event_path
       expect(page).not_to have_link "Edit", href: edit_event_path(event)
       expect(page).not_to have_button "Delete"
     end
@@ -36,6 +49,7 @@ describe "Events" do
 
       it "allows user to edit and destroy event" do
         expect(page).to have_link "RubyConf", href: event_path(event)
+        expect(page).to have_link "New Event", href: new_event_path
         expect(page).to have_link "Edit", href: edit_event_path(event)
         expect(page).to have_button "Delete"
       end
