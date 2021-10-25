a5b4a723be224cc4d80df806f10bfa1ad62e8dde
Francisco Paradella <franciscoleite.dev@protonmail.com>
2021-10-25 22:46:11 +0000

    Document the support controller

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -1,22 +1,8 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+# This controller is for general application info pages.
+# It implements the about page, documentation, and other info pages.
 class SupportController < ApplicationController
   # GET /about
-=======
-<<<<<<< HEAD
-# routes call to the Supports endpoint
-class SupportController < ApplicationController
-  # GET /about(or /)
-=======
-class SupportController < ApplicationController
-  # GET /about
->>>>>>> 0969d65 (Add support controller and about page)
->>>>>>> a2fdd89 (Add support controller and about page)
-=======
-class SupportController < ApplicationController
-  # GET /about
->>>>>>> 54e1e04 (Add support controller and about page)
   def about; end
 end
