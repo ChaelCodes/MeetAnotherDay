468125c70ef87a56c3fcc35debf26b3e8837fb08
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2023-11-02 18:07:54 +0000

    Create a notification whe you receive a friend request.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,6 +2,8 @@
 
 # This defines the relationship between two profiles.
 class Friendship < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   # I asked friend to be my buddy
   belongs_to :buddy, class_name: "Profile"
   # I was asked to become a friend by my buddy
@@ -11,6 +13,13 @@ class Friendship < ApplicationRecord
 
   validates :status, presence: true
 
+  after_create :create_notification
+
+  def create_notification
+    Notification.create(notifiable: self, profile: friend, url: friendship_url(self, only_path: true), message: self.to_s)
+  end
+
+  # Returns whichever profile is not the passed profile
   # Returns nil if they're not one of the friends
   def not_my_profile(profile)
     return friend if profile.id == buddy_id
