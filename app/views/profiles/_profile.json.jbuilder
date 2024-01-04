4f1c775db8cab0c48c0794daf98c771334602a5f
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2024-01-04 18:46:59 +0000

    Add the ability to use a gravatar for the profile image

--- app/views/profiles/_profile.json.jbuilder
+++ app/views/profiles/_profile.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! profile, :id, :name, :handle, :bio, :created_at, :updated_at
 json.url profile_url(profile, format: :json)
+json.profile_picture_url profile_picture(profile.email)
--- spec/helpers/profiles_helper_spec.rb
+++ spec/helpers/profiles_helper_spec.rb
@@ -2,16 +2,14 @@
 
 require "rails_helper"
 
-# Specs in this file have access to a helper object that includes
-# the ProfilesHelper. For example:
-#
-# describe ProfilesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ProfilesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#profile_picture" do
+    subject { helper.profile_picture(email) }
+
+    let(:email) { " TEST@example.com " }
+
+    it "chomps, lowercases, and hashes the email" do
+      is_expected.to eq "https://gravatar.com/avatar/973dfe463ec85785f5f95af5ba3906eedb2d931c24e69824a89ea65dba4e813b?s=80&d=retro&r=pg"
+    end
+  end
 end
