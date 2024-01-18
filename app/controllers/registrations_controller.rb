7d69b5ddc7c0ce9641945da721e62b68e0462932
Varun Kumar <57076268+varkum@users.noreply.github.com>
2024-01-18 10:55:23 -0500

    Redirect to new profile path after sign up and show a message (#175)
    * add devise registrations controller and message

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Define redirect route after registration for devise
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(_resource)
+    new_profile_path
+  end
+end
