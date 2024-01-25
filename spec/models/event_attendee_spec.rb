f0af464c9c20b9d083748f37997d69a41a1b7b4e
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2024-01-25 21:29:12 +0000

    Add an easier way to query for event attendees who are friends

--- spec/models/event_attendee_spec.rb
+++ spec/models/event_attendee_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe EventAttendee do
   let(:event_attendee) { create :event_attendee }
 
   it { expect(event_attendee).to be_valid }
+
+  describe ".friends_attending" do
+    subject { described_class.friends_attending(event:, profile:) }
+
+    let(:profile) { create :profile }
+    let(:event) { create :event }
+
+    it "returns nothing with no events" do
+      expect(subject.count).to eq 0
+    end
+
+    context "when attending events" do
+      let!(:event_attendee) { create :event_attendee, event:, profile: }
+
+      it { expect(subject.count).to eq 0 }
+
+      context "with friends attending" do
+        let(:friendship) { create :friendship, buddy: profile, status: :accepted }
+        let!(:friend_attendee) { create :event_attendee, event:, profile: friendship.friend }
+
+        it { expect(subject).to include(friend_attendee) }
+      end
+    end
+  end
 end
