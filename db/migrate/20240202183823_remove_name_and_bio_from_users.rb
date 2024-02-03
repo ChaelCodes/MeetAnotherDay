491a923460a3af4816d3c8917b9b7138e162af1e
francesca sandford <fran6san@gmail.com>
2024-02-03 19:11:43 +0000

    Add top-level documentation comment

--- db/migrate/20240202183823_remove_name_and_bio_from_users.rb
+++ db/migrate/20240202183823_remove_name_and_bio_from_users.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Remove name and bio as Profile already tracks name and bio
 class RemoveNameAndBioFromUsers < ActiveRecord::Migration[7.1]
   def change
     remove_column :users, :name, :string
