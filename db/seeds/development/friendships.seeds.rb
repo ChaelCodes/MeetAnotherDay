bbcfac62990b4885396dfe7431788b522e6a4a3f
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2024-03-21 16:55:44 +0000

    Update Friendship Seeds to include more examples of friendship.

--- db/seeds/development/friendships.seeds.rb
+++ db/seeds/development/friendships.seeds.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
 after "development:profiles" do
-  chael = Profile.find_by(name: "Chael's Profile")
-  john = Profile.find_by(name: "John's Profile")
+  # Establish friendships between first 10 Profiles
+  profiles = Profile.first(10)
 
-  friends = [
-    { friend_id: chael.id, buddy_id: john.id }
-  ]
+  profiles.each_cons(3) do |request_profile, profile, friend_profile|
+    Friendship.create_with(status: :accepted).find_or_create_by!(buddy_id: profile.id, friend_id: friend_profile.id)
+    Friendship.create_with(status: :requested).find_or_create_by!(buddy_id: request_profile.id, friend_id: profile.id)
+  end
 
-  friends.each do |friend|
-    Friendship.find_or_create_by(friend)
+  [
+    { buddy_id: profiles[0].id, friend_id: profiles.last.id, status: :accepted },
+    { buddy_id: profiles.last.id, friend_id: profiles[0].id, status: :requested }
+  ].each do |friendship|
+    Friendship.create_with(friendship)
+              .find_or_create_by!(buddy_id: friendship[:buddy_id], friend_id: friendship[:friend_id])
   end
 end
