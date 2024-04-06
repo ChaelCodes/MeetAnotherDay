ff53134ef122f51f50d4fe7f5866dce75ecb6aec
francesca sandford <fran6san@gmail.com>
2024-04-06 10:01:48 +0100

    Refactor migration to use `change_table`

--- db/migrate/20240202183823_remove_name_and_bio_from_users.rb
+++ db/migrate/20240202183823_remove_name_and_bio_from_users.rb
@@ -3,7 +3,9 @@
 # Remove name and bio as Profile already tracks name and bio
 class RemoveNameAndBioFromUsers < ActiveRecord::Migration[7.1]
   def change
-    remove_column :users, :name, :string
-    remove_column :users, :bio, :string
+    change_table :users, bulk: true do |t|
+      t.remove :name, type: :string
+      t.remove :bio, type: :string
+    end
   end
 end
