e93fb416a4b443b716218b0e66b40480ab4fa3c5
Gaz <gaz@lounge.com>
2024-04-18 18:21:05 +0100

    chore: upgrade to shakapacker v6

--- babel.config.js
+++ babel.config.js
@@ -1,70 +1,30 @@
-module.exports = function(api) {
-  var validEnv = ['development', 'test', 'production']
-  var currentEnv = api.env()
-  var isDevelopmentEnv = api.env('development')
-  var isProductionEnv = api.env('production')
-  var isTestEnv = api.env('test')
-
-  if (!validEnv.includes(currentEnv)) {
-    throw new Error(
-      'Please specify a valid `NODE_ENV` or ' +
-        '`BABEL_ENV` environment variables. Valid values are "development", ' +
-        '"test", and "production". Instead, received: ' +
-        JSON.stringify(currentEnv) +
-        '.'
-    )
-  }
-
-  return {
-    presets: [
-      isTestEnv && [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current'
+module.exports = function (api) {
+    const defaultConfigFunc = require('shakapacker/package/babel/preset.js')
+    const resultConfig = defaultConfigFunc(api)
+    const isProductionEnv = api.env('production')
+  
+    const changesOnDefault = {
+      presets: [
+        [
+          '@babel/preset-react',
+          {
+            development: !isProductionEnv,
+            useBuiltIns: true
           }
-        }
-      ],
-      (isProductionEnv || isDevelopmentEnv) && [
-        '@babel/preset-env',
-        {
-          forceAllTransforms: true,
-          useBuiltIns: 'entry',
-          corejs: 3,
-          modules: false,
-          exclude: ['transform-typeof-symbol']
-        }
-      ]
-    ].filter(Boolean),
-    plugins: [
-      'babel-plugin-macros',
-      '@babel/plugin-syntax-dynamic-import',
-      isTestEnv && 'babel-plugin-dynamic-import-node',
-      '@babel/plugin-transform-destructuring',
-      [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-object-rest-spread',
-        {
-          useBuiltIns: true
-        }
-      ],
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          helpers: false
-        }
-      ],
-      [
-        '@babel/plugin-transform-regenerator',
-        {
-          async: false
-        }
-      ]
-    ].filter(Boolean)
+        ]
+      ].filter(Boolean),
+      plugins: [
+        process.env.WEBPACK_SERVE && 'react-refresh/babel',
+        isProductionEnv && ['babel-plugin-transform-react-remove-prop-types',
+          {
+            removeImport: true
+          }
+        ]
+      ].filter(Boolean),
+    }
+  
+    resultConfig.presets = [...resultConfig.presets, ...changesOnDefault.presets]
+    resultConfig.plugins = [...resultConfig.plugins, ...changesOnDefault.plugins ]
+  
+    return resultConfig
   }
-}
