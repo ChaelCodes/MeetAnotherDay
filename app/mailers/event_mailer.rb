16c5b36ffc9fb7fc2341ea4bb0754d7037f765d2
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2024-05-02 19:18:06 +0000

    Set up the EventMailer!

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,4 +1,29 @@
 # frozen_string_literal: true
 
+# TODO: Is this an EventMailer, or EventAttendee Mailer?
+# Mailer responsible for email notifications around events
 class EventMailer < ApplicationMailer
+  helper EventsHelper
+
+  def pre_event_email
+    @event_attendee = params[:event_attendee]
+    @event = @event_attendee.event
+    @profile = @event_attendee.profile
+    @user = @profile.user
+    @friends_attending = friends_attending
+
+    mail(
+      to: @user.email,
+      subject: "You are attending #{@event.name}!"
+    )
+  end
+
+  private
+
+  def friends_attending
+    EventAttendeePolicy::Scope.new(
+      @user,
+      EventAttendee.friends_attending(event: @event, profile: @profile)
+    ).resolve.includes(:profile)
+  end
 end
--- spec/mailers/previews/event_preview.rb
+++ spec/mailers/previews/event_preview.rb
@@ -2,4 +2,8 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/event
 class EventPreview < ActionMailer::Preview
+  def pre_event_email
+    event_attendee = EventAttendee.find_by(id: params[:event_attendee_id]) || EventAttendee.first
+    EventMailer.with(event_attendee:).pre_event_email
+  end
 end
