c82b9bc0d5b242cbe358133ddc17a8514a3ca67a
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2025-08-07 17:57:26 +0000

    Improve EventAttendeeMailer
    - Improve text for when no friends are attending
    - Fix typo when friends are attending
    - Add spec

--- spec/mailers/event_attendee_mailer_spec.rb
+++ spec/mailers/event_attendee_mailer_spec.rb
@@ -2,6 +2,45 @@
 
 require "rails_helper"
 
+FRIENDS_ATTENDING_TEXT = "Several of your friends are also attending!"
+NO_FRIENDS_ATTENDING_TEXT = "Does the event have a Slack or Discord? Try introducing yourself over there!"
+
 RSpec.describe EventAttendeeMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#pre_event_email" do
+    subject(:mail) { described_class.with(event_attendee:).pre_event_email }
+
+    let(:event_attendee) { create :event_attendee, event:, profile: }
+    let(:profile) { create :profile, name: "Jane Doe" }
+    let(:event) { create :event, name: "RubyConf" }
+
+    it "sends an email", :aggregate_failures do
+      expect(mail.subject).to eq "You are attending RubyConf!"
+      expect(mail.to).to eq [profile.user.email]
+      expect(mail.body).to include "Jane Doe is attending RubyConf!"
+      expect(mail.body).to include NO_FRIENDS_ATTENDING_TEXT
+    end
+
+    context "with a friend attending" do
+      let(:friend) { create :profile, name: "Hercule Poirot", visibility: "everyone" }
+      let!(:friend_event_attendee) { create :event_attendee, event:, profile: friend }
+      let!(:friendship) { create :friendship, buddy: profile, friend:, status: :accepted }
+
+      it "tells you a friend is attending", :aggregate_failures do
+        # binding.irb
+        expect(mail.body).to include "Hercule Poirot"
+        expect(mail.body).to include FRIENDS_ATTENDING_TEXT
+        expect(mail.body).not_to include NO_FRIENDS_ATTENDING_TEXT
+      end
+
+      context "with friend hiding visibility" do
+        let(:friend) { create :profile, name: "Hercule Poirot", visibility: "myself" }
+
+        it "doesn't reveal event attendance", :aggregate_failures do
+          expect(mail.body).not_to include "Hercule Poirot"
+          expect(mail.body).not_to include FRIENDS_ATTENDING_TEXT
+          expect(mail.body).to include NO_FRIENDS_ATTENDING_TEXT
+        end
+      end
+    end
+  end
 end
