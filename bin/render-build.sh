283df1301b0fb1f0d3bfd3e2204c7033d5e2a211
Rachael Wright-Munn <chaelcodes@gmail.com>
2025-08-26 11:53:08 -0400

    Bundler caching (#270)
    Super fast builds and deploys!
    
    * Create a bundle config that will automatically cache.
    
    * Updated ruby/setup-ruby to use bundler-cache.
    
    ruby version is unnecessary if you have a .ruby-version file.
    Remove bundle install from copilot dev env - it's aleady run by setup-ruby.
    
    * Remove docker from github action.
    
    * Turns out we weren't using the selenium image.
    
    * Speed up render deploys by caching and excluding development and test gems.
    
    * Update to the latest version of actions/checkout
    
    ---------
    
    Co-authored-by: Rachael Wright-Munn <hello@chael.codes>

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # ensures nokogiri uses ruby instead of native extensions so the render build will succeed.
 bundle config set force_ruby_platform true
 
-bundle install
+bundle install --deployment --without development test
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rake db:migrate
