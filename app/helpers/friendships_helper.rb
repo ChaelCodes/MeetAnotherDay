cbf63154d140def74ae47c86c916302cff3272c1
Rachael Wright-Munn <hello@chael.codes>
2025-08-28 19:39:51 +0000

    Add relationship_buttons to FriendshipHelper

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -8,7 +8,6 @@ module FriendshipsHelper
     safe_join([
       accept_friendship_button(friendship, current_profile),
       befriend_button(friendship, current_profile),
-      destroy_friendship_button(friendship, current_profile),
       request_friend_button(friendship, current_profile),
       block_button(friendship, current_profile)
     ].compact)
@@ -22,9 +21,9 @@ module FriendshipsHelper
                                                          friendship: {
                                                            status: :accepted
                                                          }
-                                                       }), form_class: "control",
+                                                       }), form_class: "card-footer-item p-0 control",
                                                            method: :put,
-                                                           class: "button is-success is-outlined"
+                                                           class: "button is-success is-fullwidth is-radiusless"
   end
 
   def befriend_button(friendship, current_profile)
@@ -35,9 +34,9 @@ module FriendshipsHelper
                                  friend_id: friendship.friend_id,
                                  buddy_id: friendship.buddy_id,
                                  status: :accepted
-                               }), form_class: "control",
+                               }), form_class: "card-footer-item p-0 control",
                                    method: :post,
-                                   class: "button is-success"
+                                   class: "button is-success is-fullwidth is-radiusless"
   end
 
   # Block a Friendship - Create if no Friendship exists, Update if it does
@@ -50,37 +49,61 @@ module FriendshipsHelper
                 buddy_id: friendship.buddy_id,
                 friend_id: friendship.friend_id
               } },
-              form_class: "control",
-              class: "button is-danger is-outlined"
+              form_class: "card-footer-item p-0 control",
+              class: "button is-danger is-outlined is-fullwidth is-radiusless"
+  end
+
+  # Cancel a friend request
+  def cancel_friendship_button(relationship)
+    return unless relationship.request_sent?
+    button_to "Cancel friend request",
+              friendship_path(relationship.other_friendship),
+              form_class: "card-footer-item p-0 control",
+              class: "button is-warning is-outlined is-fullwidth is-radiusless",
+              method: :delete
   end
 
   # This displays a button to destroy the friendship
-  def destroy_friendship_button(friendship, current_profile)
-    return unless current_profile == friendship.buddy
+  def destroy_friendship_button(relationship)
+    friendship = relationship.friendship
     return unless friendship.persisted?
+    return if relationship.request_sent?
     button_names = { "accepted" => "Unfriend", "blocked" => "Unblock", "requested" => "Ignore" }
     button_to button_names[friendship.status],
               friendship_path(friendship),
-              form_class: "control",
-              class: "button is-warning is-outlined",
+              form_class: "card-footer-item p-0 control",
+              class: "button is-warning is-outlined is-fullwidth is-radiusless",
               method: :delete
   end
 
   # Request the Friendship of another user
-  def request_friend_button(friendship, current_profile)
-    return if friendship.persisted?
-    return unless friendship.friend == current_profile
+  def request_friend_button(relationship)
+    return if relationship.other_friendship.persisted?
+    return unless relationship.friendship.accepted?
+
     button_to "Request Friend",
               friendships_path(friendship: {
-                                 friend_id: friendship.friend_id,
-                                 buddy_id: friendship.buddy_id,
+                                 friend_id: relationship.profile.id,
+                                 buddy_id: relationship.other_profile.id,
                                  status: "requested"
                                }), method: :post,
-                                   form_class: "control",
-                                   class: "button is-primary is_outlined"
+                                   form_class: "card-footer-item p-0 control",
+                                   class: "button is-primary is-fullwidth is-radiusless"
   end
 
   def view_friendship(friendship)
     link_to friendship
+  end
+
+  def relationship_buttons(relationship)
+    return nil unless relationship
+    safe_join([
+      befriend_button(relationship.friendship, relationship.profile),
+      accept_friendship_button(relationship.friendship, relationship.profile),
+      request_friend_button(relationship),
+      cancel_friendship_button(relationship),
+      destroy_friendship_button(relationship),
+      block_button(relationship.friendship, relationship.profile)
+    ].compact)
   end
 end
--- spec/helpers/friendships_helper_spec.rb
+++ spec/helpers/friendships_helper_spec.rb
@@ -101,71 +101,199 @@ RSpec.describe FriendshipsHelper do
     end
   end
 
-  describe "#destroy_friendship_button" do
-    subject { destroy_friendship_button(friendship, current_profile) }
+  describe "#cancel_friendship_button" do
+    subject { cancel_friendship_button(relationship) }
 
-    let(:current_user) { create :user }
-    let(:current_profile) { create :profile, user: current_user }
+    let(:profile) { create :profile }
+    let(:other_profile) { create :profile }
+    let(:relationship) { Relationship.new(profile:, other_profile:) }
 
-    context "when it's not your friendship" do
-      let(:friendship) { create :friendship }
+    context "when friend request sent" do
+      let!(:friendship) { create :friendship, status: :accepted, buddy: profile, friend: other_profile }
+      let!(:other_friendship) { create :friendship, status: :requested, buddy: other_profile, friend: profile }
 
-      it { is_expected.to be_nil }
+      it { is_expected.to match(/Cancel friend request/) }
     end
 
-    context "when friendship isn't saved" do
-      let(:friendship) { build :friendship, buddy: current_profile }
+    context "when friend request received" do
+      let!(:friendship) { create :friendship, status: :requested, buddy: profile, friend: other_profile }
+      let!(:other_friendship) { create :friendship, status: :accepted, buddy: other_profile, friend: profile }
 
+      it { is_expected.not_to match(/Cancel friend request/) }
+    end
+  end
+
+  describe "#destroy_friendship_button" do
+    subject { destroy_friendship_button(relationship) }
+
+    let(:relationship) { Relationship.new(profile:, other_profile:) }
+    let(:profile) { create :profile }
+    let(:other_profile) { create :profile }
+
+    context "when friendship isn't saved" do
       it { is_expected.to be_nil }
     end
 
     context "when friendship is accepted" do
-      let(:friendship) { create :friendship, buddy: current_profile, status: :accepted }
+      let!(:friendship) { create :friendship, buddy: profile, friend: other_profile, status: :accepted }
 
       it { is_expected.to match(/Unfriend/) }
     end
 
     context "when friendship is blocked" do
-      let(:friendship) { create :friendship, buddy: current_profile, status: :blocked }
+      let!(:friendship) { create :friendship, buddy: profile, friend: other_profile, status: :blocked }
 
       it { is_expected.to match(/Unblock/) }
     end
 
     context "when friendship is requested" do
-      let(:friendship) { create :friendship, buddy: current_profile, status: :requested }
+      let!(:friendship) { create :friendship, buddy: profile, friend: other_profile, status: :requested }
 
       it { is_expected.to match(/Ignore/) }
+    end
+
+    context "when friend request has been sent" do
+      let!(:friendship) { create :friendship, buddy: profile, friend: other_profile, status: :accepted }
+      let!(:other_friendship) { create :friendship, buddy: other_profile, friend: profile, status: :requested }
+
+      it { is_expected.to be_nil }
     end
   end
 
   describe "#request_friend_button" do
-    subject { request_friend_button(friendship, current_profile) }
+    subject { request_friend_button(relationship) }
 
-    let(:current_user) { create :user }
-    let(:current_profile) { create :profile, user: current_user }
+    let(:relationship) { Relationship.new(profile:, other_profile:) }
+    let(:profile) { create :profile }
+    let(:other_profile) { create :profile }
 
-    context "when friendship already exists" do
-      let(:friendship) { create :friendship, friend: current_profile }
+    it { is_expected.to be_nil }
+
+    context "when you are already friends" do
+      let!(:friendship) { create :friendship, status: :accepted, buddy: profile, friend: other_profile }
+      let!(:other_friendship) { create :friendship, status: :accepted, buddy: other_profile, friend: profile }
 
       it { is_expected.to be_nil }
+    end
 
-      context "when it is my friendship" do
-        let(:friendship) { create :friendship, buddy: current_profile }
+    context "when there is a friend request" do
+      let!(:friendship) { create :friendship, status: :accepted, buddy: profile, friend: other_profile }
+      let!(:other_friendship) { create :friendship, status: :requested, buddy: other_profile, friend: profile }
 
-        it { is_expected.to be_nil }
+      it { is_expected.to be_nil }
+    end
+
+    context "when you are friendly" do
+      let!(:friendship) { create :friendship, status: :accepted, buddy: profile, friend: other_profile }
+
+      it { is_expected.to match(/Request Friend/) }
+    end
+
+    context "when you blocked them" do
+      let!(:friendship) { create :friendship, status: :blocked, buddy: profile, friend: other_profile }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe "#relationship_buttons" do
+    subject { relationship_buttons(relationship) }
+
+    let(:profile) { create :profile }
+    let(:other_profile) { create :profile }
+    let(:relationship) { Relationship.new(profile:, other_profile:) }
+
+    context "when no relationship", :aggregate_failures do
+      it { is_expected.to match(/Befriend/) }
+      it { is_expected.not_to match(/Request Friend/) }
+      it { is_expected.not_to match(/Cancel friend request/) }
+      it { is_expected.not_to match(/Accept friend request/) }
+      it { is_expected.not_to match(/Unfriend/) }
+      it { is_expected.to match(/Block/) }
+    end
+
+    context "when friends", :aggregate_failures do
+      let!(:friendship) { create :friendship, buddy: profile, friend: other_profile, status: :accepted }
+      let!(:other_friendship) { create :friendship, buddy: other_profile, friend: profile, status: :accepted }
+
+      it { is_expected.not_to match(/Befriend/) }
+      it { is_expected.not_to match(/Request Friend/) }
+      it { is_expected.not_to match(/Cancel friend request/) }
+      it { is_expected.not_to match(/Accept friend request/) }
+      it { is_expected.to match(/Unfriend/) }
+      it { is_expected.to match(/Block/) }
+    end
+
+    context "when you have sent a friend request" do
+      let!(:friendship) { create :friendship, buddy: profile, friend: other_profile, status: :accepted }
+      let!(:other_friendship) { create :friendship, buddy: other_profile, friend: profile, status: :requested }
+
+      it { is_expected.not_to match(/Befriend/) }
+      it { is_expected.not_to match(/Request Friend/) }
+      it { is_expected.not_to match(/Unfriend/) }
+      it { is_expected.to match(/Cancel friend request/) }
+      it { is_expected.not_to match(/Accept friend request/) }
+      it { is_expected.to match(/Block/) }
+    end
+
+    context "when you are friendly" do
+      let!(:friendship) { create :friendship, buddy: profile, friend: other_profile, status: :accepted }
+
+      it { is_expected.not_to match(/Befriend/) }
+      it { is_expected.to match(/Request Friend/) }
+      it { is_expected.to match(/Unfriend/) }
+      it { is_expected.not_to match(/Cancel friend request/) }
+      it { is_expected.not_to match(/Accept friend request/) }
+      it { is_expected.to match(/Block/) }
+    end
+
+    context "when you have received a friend request" do
+      let!(:friendship) { create :friendship, buddy: profile, friend: other_profile, status: :requested }
+
+      it { is_expected.not_to match(/Befriend/) }
+      it { is_expected.not_to match(/Request Friend/) }
+      it { is_expected.not_to match(/Unfriend/) }
+      it { is_expected.not_to match(/Cancel friend request/) }
+      it { is_expected.to match(/Accept friend request/) }
+      it { is_expected.to match(/Ignore/) }
+      it { is_expected.to match(/Block/) }
+    end
+
+    context "when they have blocked you" do
+      let!(:friendship) { create :friendship, buddy: other_profile, friend: profile, status: :blocked }
+
+      it { is_expected.to match(/Befriend/) }
+      it { is_expected.not_to match(/Request Friend/) }
+      it { is_expected.not_to match(/Unfriend/) }
+      it { is_expected.not_to match(/Cancel friend request/) }
+      it { is_expected.not_to match(/Accept friend request/) }
+      it { is_expected.not_to match(/Ignore/) }
+      it { is_expected.to match(/Block/) }
+
+      context "when you are friendly" do
+        let!(:your_friendship) { create :friendship, buddy: profile, friend: other_profile, status: :accepted }
+
+        it { is_expected.not_to match(/Befriend/) }
+        it { is_expected.not_to match(/Request Friend/) }
+        it { is_expected.to match(/Unfriend/) }
+        it { is_expected.not_to match(/Cancel friend request/) }
+        it { is_expected.not_to match(/Accept friend request/) }
+        it { is_expected.not_to match(/Ignore/) }
+        it { is_expected.to match(/Block/) }
       end
     end
 
-    context "when friendship does not exist" do
-      let(:friendship) { build :friendship, friend: current_profile }
+    context "when you have blocked them" do
+      let!(:friendship) { create :friendship, buddy: profile, friend: other_profile, status: :blocked }
 
-      it { is_expected.to match(/Request Friend/) }
-
-      context "when it is my friendship" do
-        let(:friendship) { build :friendship, buddy: current_profile }
-
-        it { is_expected.to be_nil }
-      end
+      it { is_expected.not_to match(/Befriend/) }
+      it { is_expected.not_to match(/Request Friend/) }
+      it { is_expected.not_to match(/Unfriend/) }
+      it { is_expected.not_to match(/Cancel friend request/) }
+      it { is_expected.not_to match(/Accept friend request/) }
+      it { is_expected.not_to match(/Ignore/) }
+      it { is_expected.not_to match(/Block/) }
+      it { is_expected.to match(/Unblock/) }
     end
   end
 end
