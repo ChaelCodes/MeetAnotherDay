e9806428f600ce4bd3f94b2724da281231f2719d
Rachael Wright-Munn <hello@chael.codes>
2025-08-30 19:41:00 +0000

    Turn on mocks in preview environments. Cloudflare doesn't support wildcard hostnames.

--- config/initializers/cloudflare_turnstile.rb
+++ config/initializers/cloudflare_turnstile.rb
@@ -4,4 +4,5 @@ RailsCloudflareTurnstile.configure do |c|
   c.site_key = ENV.fetch("CLOUDFLARE_SITE_KEY", "")
   c.secret_key = ENV.fetch("CLOUDFLARE_SECRET_KEY", "")
   c.fail_open = true
+  c.mock_enabled = !Rails.env.production? || ENV.fetch("IS_PULL_REQUEST") == "true"
 end
