443d2e7957a2ae980bd67ceff94f723f7e9321d5
Rachael Wright-Munn <hello@chael.codes>
2025-09-09 17:02:09 +0000

    fixup! Turn off 'force_ruby_platform=true' - Herb fails to install with it active.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,6 @@
 # exit on any error or unset variable
 set -euo pipefail
 
-# ensures nokogiri uses ruby instead of native extensions so the render build will succeed.
-bundle config set force_ruby_platform true
-
 bundle install --deployment --without development test
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
