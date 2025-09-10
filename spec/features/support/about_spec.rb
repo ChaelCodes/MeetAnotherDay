e5ab5d0fbea6651b9f61c4681f3b904d2b769556
Rachael Wright-Munn <hello@chael.codes>
2025-09-10 14:08:51 +0000

    Fix RSpec/ExcessiveDocstringSpacing

--- spec/features/support/about_spec.rb
+++ spec/features/support/about_spec.rb
@@ -7,7 +7,7 @@ describe "Support" do
     visit(+"/")
   end
 
-  it "shows the " do
+  it "shows the about page" do
     expect(page).to have_content "It was the first day of Strangeloop, and Chael was having lunch alone."
   end
 end
